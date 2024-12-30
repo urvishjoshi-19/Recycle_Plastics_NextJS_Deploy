e8b686e14561c332ee2ed90e094b1108357342e5
urvishjoshi-19 <urvishwork19@gmail.com>
2024-12-30 13:10:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-}
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'), // Replace 'src' with the base directory of your components if different
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
